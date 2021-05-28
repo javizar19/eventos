f9db5351af08de4c868baa565bf342b80406dcbb
Javier Zarco <jzb00@iesemilidarder.com>
2021-05-28 18:40:04 +0200

    se pone el json y el login con el localStorage

--- js/app.js
+++ js/app.js
@@ -1,88 +1,56 @@
-const containerDiv = document.querySelector('#list-events');
-let eventSelect = document.querySelector("#filtrar-estrellas");
-const buscarBtn = document.querySelector("#search-btn");
-let eventXML = "";
 
 var myModal = new bootstrap.Modal(document.getElementById('modal-edicion'), {
     keyboard: false
   });
-
-  window.onload = function () {
-    cargarXMLEvent();
-    getLista();
-    buscarBtn.addEventListener("click", (event) => {
-        filtrarEvents();
-    });
-};
-
     	
-const cargarXMLEvent = () => {
-  fetch("./data/hoteles.xml")
-      .then(response => response.text())
-      .then(texto => (new window.DOMParser()).parseFromString(texto, "text/xml"))
-      .then(datos => {
-          eventXML = datos;
-          pintaEvents(eventXML.querySelectorAll('hotel'))
-      })
-      .catch(Error => {
-          alert("Archivo no encontrado");
-          console.log(Error);
-      });
-};
 
+function init() {
+    // Inicialment mostram totes les figures
+    getFigures("all");
 
-const pintaEvents = (listaEvents) => {
-  containerDiv.innerHTML = "";
-  listaEvents.forEach(event => {
-      let nombre = event.querySelector("nombre").textContent;
-      let descripcion = event.querySelector("descripcion").textContent;
-      let estrellas = event.querySelector("estrellas").textContent;
-      let comentarios = event.querySelector("comentarios").textContent;
-      let destacado = event.getAttribute("destacado");
-      let puntuacion = event.querySelector("puntuacion").textContent;
-      let img = event.querySelector("fotoPortada").textContent;
-      let municipio = event.querySelector("municipio").textContent;
-      let servicios = event.querySelector("servicios").textContent;
-
-      containerDiv.innerHTML += pintaEventCard({
-          nombre,
-          descripcion,
-          estrellas,
-          comentarios,
-          destacado,
-          img,
-          municipio,
-          servicios,
-          puntuacion
-      });
-  })
+    let buttonsList = document.querySelectorAll('.btn-check');
+    for (const button of buttonsList) {
+        button.addEventListener("click", function () {
+            getFigures(this.value);
+        });
+    }
 }
 
+function getFigures(pType) {
+    // Petició asíncrona
+    fetch('./data/figures.json')
+        .then(response => response.json())
+        .then((collection) => {
+            console.log("name", collection.nameCollection);
+            pintarListaFiguras(collection.figures, pType);
 
-const pintaEventCard = (event) => {
-  return `
-                  <div class="card" style="width: 18rem;">
-                          <img class="card-img-top" src="./img/${event.img}" alt="Card image cap" style="width:286px height:120px;height: 210px;">
-                      <div class="card-body">
-                          <h5 class="card-title">${event.nombre}</h5>
-                          <p class="card-text">${event.descripcion}</p>
-                          <a href="#" class="btn btn-primary">Click and buy</a>
-                       </div>
-                  </div>
-  `
+        });
 }
 
-
-const filtrarEvents = () => {
-  let eventSeleccionado = eventSelect.options[eventSelect.selectedIndex].value;
-  if(eventXML!=""){
-      const listaEvents = eventXML.querySelectorAll('hotel');
-      let listaFiltrada=[];
-      listaEvents.forEach(event => {
-          if (event.querySelector("estrellas").textContent == eventSeleccionado || eventSeleccionado==""){
-              listaFiltrada.push(event);
-          }
-      });
-      pintaEvents(listaFiltrada);
-  };
+const pintarListaFiguras = (listaFiguras, pType) => {
+    const container = document.getElementById("card-container");
+    container.innerHTML = "";
+    for (let figure of listaFiguras) {
+        if (pType == figure.type || pType=="all") {
+            container.innerHTML += `
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/${figure.img}" width="100%" alt="tanos image">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">${figure.name}</h5>
+                                <p class="card-text">${figure.description}</p>
+                                <p class="card-text"><small class="text-muted">Precio ${figure.price}€</small></p>
+                                <a href="#" class="btn btn-primary">Comprar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
+    }
 };
+
+init();
---
+++ js/loginregister.js
@@ -0,0 +1,29 @@
+let name = document.getElementById('name1');
+let pw = document.getElementById('pw');
+
+
+function store() {
+    localStorage.setItem('name1', name1.value);
+    localStorage.setItem('pw', pw.value);
+}
+
+
+function check() {
+
+
+    let storedName1 = localStorage.getItem('name1');
+    let storedPw = localStorage.getItem('pw');
+
+
+    let userName1 = document.getElementById('userName1');
+    let userPw = document.getElementById('userPw');
+
+    if(userName1.value == storedName1 && userPw.value == storedPw) {
+        alert('Logueado correctamente')
+       window.location.assign('eventos-form.html');
+       alert('Logueado correctamente')
+      
+    }else {
+        alert('Usuario no registrado');
+    }
+}
